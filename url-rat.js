165c59f622314104e795e6e8d72b5f47c56d7cae
John Colagioia <john@colagioia.net>
2020-06-16 06:56:11 -0400

    Replace border-drawing code with reporting URLs to localhost

--- url-rat.js
+++ url-rat.js
@@ -1,1 +1,27 @@
-document.body.style.border = "5px solid red";
+// You'll need a server of your own to capture these.
+// Change the URL below to match.
+var url = 'http://localhost:8080/';
+var currentUrl = document.location.href;
+
+console.log(currentUrl);
+(async () => {
+  // Send the request
+  const rawResponse = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Accept': 'text/plain',
+      'Content-Type': 'text/html',
+      // My sample server isn't recognizing the request body,
+      // so since it's just a URL, I'm stashing a copy in the
+      // header, too.
+      'X-This-Is-The-Url': currentUrl
+    },
+    body: currentUrl
+  });
+  // Get the response
+  const content = await rawResponse;
+  // Dump the response to the console, so that we can tell
+  // something happened on our side.
+  console.log(content);
+})();
+
